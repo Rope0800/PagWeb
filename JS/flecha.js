887e3c7f2cffb86c6f0c6d4eedcee33e687622d2
Rope0800 <santiagounigomez@gmail.com>
2023-10-25 11:38:19 -0300

    retoques

--- JS/flecha.js
+++ JS/flecha.js
@@ -6,26 +6,8 @@ document.getElementById('logo').addEventListener('click', function() {
 });
 });
 
-const imagenes = ["JS/Galeria/tony.jpg", "imagen2.jpg", "imagen3.jpg"]; // Agrega las rutas de tus imágenes
-let indice = 0; // Índice de la imagen actual
-const imagenCarrusel = document.getElementById("imagenCarrusel");
 
-function mostrarImagen() {
-    imagenCarrusel.src = imagenes[indice];
-}
 
-function avanzar() {
-    indice++;
-    if (indice >= imagenes.length) {
-        indice = 0; // Vuelve al principio cuando llega al final
-    }
-    mostrarImagen();
-}
 
-function retroceder() {
-    indice--;
-    if (indice < 0) {
-        indice = imagenes.length - 1; // Va al último cuando retrocede desde el primero
-    }
-    mostrarImagen();
-}
+
+
